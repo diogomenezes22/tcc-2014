9e795d7df193355d8690f87317c8f73561cadc39
devilallstar <devilallstar@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-09-04 13:54:20 +0000

    beans.xml adicionado

--- trunk/tcc-project/tcc/src/main/java/br/edu/granbery/tcc/controller/GameController.java
+++ trunk/tcc-project/tcc/src/main/java/br/edu/granbery/tcc/controller/GameController.java
@@ -1,32 +1,29 @@
 package br.edu.granbery.tcc.controller;
 
-import java.util.Random;
-
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.edu.granbery.tcc.view.GameControllerView;
 
 @Named
-@ViewScoped
+@SessionScoped
 public class GameController {
 	
 	@Inject
-	GameControllerView view;
+	public GameControllerView view;
 	
 	@PostConstruct
-	public void inicializar(){
+	public void load(){
 		System.out.println("testando...........");
 		System.out.println("1...........");
 		System.out.println("2...........");
 		System.out.println("3...........");
 	}
 	
-	public int sortearDado(){
-		Random r = new Random();
-		
-		return r.nextInt(6) + 1;
+	public String sortearDado(){
+		System.out.println("Chupa!!!");
+		return "/game.xhtml";
 	}
 }
