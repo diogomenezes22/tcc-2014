6b742eecebafca250ae3a9db307020f2347e73e2
devilallstar <devilallstar@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-09-05 18:46:23 +0000

    libs rich faces

--- trunk/tcc-project/tcc/src/main/java/br/edu/granbery/tcc/controller/GameController.java
+++ trunk/tcc-project/tcc/src/main/java/br/edu/granbery/tcc/controller/GameController.java
@@ -1,29 +1,58 @@
 package br.edu.granbery.tcc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.edu.granbery.model.Resposta;
 import br.edu.granbery.tcc.view.GameControllerView;
 
 @Named
-@SessionScoped
+@ViewScoped
 public class GameController {
 	
 	@Inject
-	public GameControllerView view;
-	
-	@PostConstruct
+	private GameControllerView view;
+
 	public void load(){
-		System.out.println("testando...........");
-		System.out.println("1...........");
-		System.out.println("2...........");
-		System.out.println("3...........");
+		this.view.setMostrarPergunta(false);
+		//this.view.setVariavelTeste("Por que esta meerda não aparece na tela");
+	}
+	@PostConstruct
+	public void buscarPergunta(){
+		//mock temporario para testes
+		this.view.setVariavelTeste("Por que esta buceta não aparece na tela");
+		Resposta r1 = new Resposta();
+		Resposta r2 = new Resposta();
+		Resposta r3 = new Resposta();
+		Resposta r4 = new Resposta();
+		r1.setDescricao("Sim");
+		r1.setCorreta(false);
+		r2.setDescricao("Com certeza");
+		r2.setCorreta(false);
+		r3.setDescricao("óbvio");
+		r3.setCorreta(false);
+		r4.setDescricao("todas as alternativas anteriores");
+		r4.setCorreta(true);
+		List<Resposta> respostas = new ArrayList<Resposta>();
+		respostas.add(r4);
+		respostas.add(r3);
+		respostas.add(r2);
+		respostas.add(r1);
+		this.view.getPergunta().setDescricao("O renato e gay ?");
+		this.view.getPergunta().setRespostas(respostas);
+		this.view.setMostrarPergunta(true);
 	}
 	
-	public String sortearDado(){
-		System.out.println("Chupa!!!");
-		return "/game.xhtml";
+	public GameControllerView getView() {
+		return view;
+	}
+	
+	public void setView(GameControllerView view) {
+		this.view = view;
 	}
 }
