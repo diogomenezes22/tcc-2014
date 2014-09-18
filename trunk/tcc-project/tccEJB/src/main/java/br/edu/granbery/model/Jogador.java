3547ac14df66d3a231aeeaa53916cf2662b3a8cf
rodriguim22@gmail.com <rodriguim22@gmail.com@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-09-18 01:54:36 +0000

    Ajustes no pom

--- trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/model/Jogador.java
+++ trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/model/Jogador.java
@@ -2,17 +2,12 @@ package br.edu.granbery.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.ejb.Local;
+import javax.persistence.*;
 
 @Entity
 @Table(name="jogador")
+@Local
 public class Jogador implements Serializable{
 
 	/**
