d30647e169210b89db3229ab209b5e6a0b4a7099
rodriguim22@gmail.com <rodriguim22@gmail.com@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-09-23 22:37:47 +0000

    Criação de alguns DAO's e managers

---
+++ trunk/tcc-project/tcc/src/main/java/br/edu/granbery/tcc/controller/PerguntaRespostaController.java
@@ -0,0 +1,45 @@
+package br.edu.granbery.tcc.controller;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.granbery.tcc.manager.PerguntaManager;
+import br.edu.granbery.tcc.model.Pergunta;
+
+@Named
+@RequestScoped
+public class PerguntaRespostaController {
+	
+	private Pergunta pergunta;
+	
+	@Inject
+	PerguntaManager perguntaManager;
+	
+	@PostConstruct
+	public void load(){
+		pergunta = new Pergunta();		
+	}
+
+	public Pergunta getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(Pergunta pergunta) {
+		this.pergunta = pergunta;
+	}
+	
+	public String salvar(){
+		perguntaManager.salvar(pergunta);
+		pergunta = new Pergunta();
+		return "salvo";
+	}
+	
+	public void buscar(){
+		perguntaManager.salvar(pergunta);
+		pergunta = new Pergunta();
+		return ;
+	}
+
+}
