f5b1d21e9d4dd8d7f4a951a86776123d95e5a54d
devilallstar <devilallstar@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-09-26 17:22:28 +0000

    Converter Generico

--- tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Jogador.java
+++ tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Jogador.java
@@ -9,15 +9,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import br.edu.granbery.tcc.util.SampleEntity;
+
 @Entity
 @Table(name="TB_JOGADOR")
-public class Jogador implements Serializable{
-
+public class Jogador implements Serializable, SampleEntity{
+	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -5445385721352730525L;
-	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="ID_JOGADOR")
@@ -55,6 +57,30 @@ public class Jogador implements Serializable{
 	}
 	public void setIdade(Integer idade) {
 		this.idade = idade;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jogador other = (Jogador) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
