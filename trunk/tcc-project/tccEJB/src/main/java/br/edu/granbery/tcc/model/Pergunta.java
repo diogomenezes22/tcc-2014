1ed53201b9de33415b6cfe77cf664e9c41221430
devilallstar <devilallstar@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-11-02 16:55:40 +0000

    Pergunta e resposta e passagem de vez com sucesso

--- trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Pergunta.java
+++ trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Pergunta.java
@@ -58,5 +58,30 @@ public class Pergunta implements Serializable{
 	public void setRespostas(List<Resposta> respostas) {
 		this.respostas = respostas;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pergunta other = (Pergunta) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
