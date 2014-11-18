9f683b068787a2b63cee38293689d87a20ff72cd
rodriguim22@gmail.com <rodriguim22@gmail.com@9dbecd28-41af-1590-1a44-61ad4d5ce066>
2014-11-18 00:02:22 +0000

    Correção do CRUD de Pergunta e do componente Radio

--- trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Resposta.java
+++ trunk/tcc-project/tccEJB/src/main/java/br/edu/granbery/tcc/model/Resposta.java
@@ -43,7 +43,7 @@ public class Resposta implements Serializable, SampleEntity{
 	public Resposta(Pergunta perg){
 		pergunta = perg;
 		descricao = "";
-		correta = "ERRADA";
+		correta = ERRADA;
 	}
 	
 	public Resposta(){
